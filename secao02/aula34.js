2e144dd01f66b36975c0cf848776df22cd65d91e
DannyElis89 <dannyelis89@gmail.com>
2023-04-05 17:25:33 -0300

    formata documento

--- secao02/aula34.js
+++ secao02/aula34.js
@@ -1,25 +1,24 @@
-function meuEscopo () {
-  const form = document.querySelector('.form');
-  const resultado = document.querySelector('.resultado');
+function meuEscopo() {
+  const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
 
   const pessoas = [];
 
-  function recebeEventoForm (evento) {
+  function recebeEventoForm(evento) {
     evento.preventDefault();
 
-    const nome = form.querySelector('.nome').value;
-    const sobrenome = form.querySelector('.sobrenome').value;
-    const peso = form.querySelector('.peso').value;
-    const altura = form.querySelector('.altura').value;
+    const nome = form.querySelector(".nome").value;
+    const sobrenome = form.querySelector(".sobrenome").value;
+    const peso = form.querySelector(".peso").value;
+    const altura = form.querySelector(".altura").value;
 
-    pessoas.push({nome, sobrenome, peso, altura});
+    pessoas.push({ nome, sobrenome, peso, altura });
 
     console.log(pessoas);
 
-    resultado.innerHTML += `<p>${nome} ${sobrenome} ` +
-      `${peso} ${altura}</p>`;
+    resultado.innerHTML += `<p>${nome} ${sobrenome} ` + `${peso} ${altura}</p>`;
   }
 
-  form.addEventListener('submit', recebeEventoForm);
+  form.addEventListener("submit", recebeEventoForm);
 }
 meuEscopo();
